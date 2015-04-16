9cf090e80e21e626f5ed100bfb481a73d4aa9152
kestein <kestein42932@gmail.com>
2015-04-16 13:11:01 -0700

    Finished the script to monitor which twitch streamers are online

--- db/twitch.js
+++ db/twitch.js
@@ -6,41 +6,58 @@ var https = require("https");
 var database = "mongodb://localhost:27017/riotContest";
 var dbPlayers = "Players";
 var dbOnline = "Online";
+var lolGame = "League of Legends"
+var apiDelay = 1200;
 var twitchAPI = "https://api.twitch.tv/kraken/streams?channel=";
 //var client = new MongoClient(new Server("localhost", 27017));
 
-/* Pull the list of twitch users from the "Players" database */
+/* Pull the list of twitch users from the "Players" database
+INPUT: Nothing.
+OUTPUT: Feeds the list of streamers from the "Players" database to processTwitchRequest. */
 function getTwitchUsernames() {
    mongoClient.connect(database, function(err, db) {
       if(err) {
-         return err;
+         /* Handle the error */
+         handleDbError(err);
       }
       var docs = [];
       var collection = db.collection(dbPlayers);
       var playersCursor = collection.find();
       playersCursor.toArray(function(err, documents) {
-         if(err == null) {
-            /* Use the twitch API  */
-            for(var y = 0; y < documents.length; y++) {
-               /* Call a function to query twitch because SOFTWARE DESIGN */
-               /* Wait some time so I don't DdoS twitch.tv */
-               (function(i) {
-                  var a = (i+1)*1000;
-                  setTimeout(function() {
-                     processTwitchRequest(documents[i])
-                  }, (y+1)*1200);
-               })(y);
+         if(err) {
+            /* Handle the error */
+            handleCursorError(err);
+         }
+         /* Ensure that the database resource is freed only after the 
+            entire cursor has been iterated through*/
+         var docs = documents.length;
+         /* End of the line */
+         function checkDocOff() {
+            docs--;
+            if(docs == 0) {
+               db.close();
+               console.log("done");
             }
          }
-         else {
-            /* Mongo did not return anything, move along, log it */
+         /* Use the twitch API  */
+         for(var y = 0; y < documents.length; y++) {
+            /* Call a function to query twitch because SOFTWARE DESIGN */
+            /* Wait some time so I don't DdoS twitch.tv */
+            (function(i) {
+               setTimeout(function() {
+                  processTwitchRequest(documents[i]);
+               }, (i+1)*apiDelay);
+               checkDocOff();
+            })(y);
          }
       });
    });
 }
 
 /* Uses the twitch API to see if a given twitch streamer is online. Modifies
-   the "Online" database.*/
+   the "Online" database. 
+   INPUT: A document containing information from the "Players" database.
+   OUTPUT: Maintains the "Online" database with twitch streamers that are online playing League. */
 function processTwitchRequest(data) {
    var twitchRequest = https.get(twitchAPI + data.twitchUsername, function(res) {
       var twitchData = '';
@@ -51,16 +68,53 @@ function processTwitchRequest(data) {
       /* Handle if a streamer is on or offline */
       res.on('end', function() {
          var parsedTwitchData = JSON.parse(twitchData);
-         /* Since I am querying individual channels, _total will only be 
-            0 or 1*/
-         if(parsedTwitchData._total > 0) {
-            console.log(data.twitchUsername + " is online!");
-         }
-         else {
-            console.log(data.twitchUsername + " is offline.");
-         }
+         mongoClient.connect(database, function(err, db) {
+            if(err) {
+               /* Handle the error */
+               handleDbError(err);
+            }
+            var collection = db.collection(dbOnline);
+            /* Make a function to close the database. This way the db can be closed from the parent scope when we KNOW
+               the children are done. */
+            function closeDb() {
+               db.close();
+            }
+            /* User is online and playing League. See if they need to be added to the "Online" database */
+            if(parsedTwitchData._total > 0 && parsedTwitchData.streams[0].game == lolGame) {
+               /* use the data from twitch to see if this user is already in the "Online" database */
+               var onlineCursor = collection.find({twitchUsername: parsedTwitchData.streams[0].channel.display_name});
+               onlineCursor.nextObject(function(err, item) { 
+                  if(err) {
+                     /* Handle the error */
+                     handleCursorError(err);
+                  }
+                  /* They are already in the "Online" database. Do nothing */
+                  if(item != null) {
+                     /* TODO: make a priority queue and give this twitch streamer the lowest priority */
+                  }
+                  /* item is null. The user just came online, add them to the "Online" database */
+                  else {
+                     collection.insert(data);
+                  }
+                  closeDb();
+               });
+            }
+            /* The user is not online or not playing League. Remove them from the "Online" database */
+            else {
+               collection.remove(data);
+               closeDb();
+            }
+         });
       });
    });
+}
+
+function handleCursorError(err) {
+   console.log(err);
+}
+
+function handleDbError(err) {
+   console.log(err);
 }
 
 getTwitchUsernames();
