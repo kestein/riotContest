c91e3ec36c9d3c68e46cbba9f7c193ff0dfdab39
kestein <kestein42932@gmail.com>
2015-04-17 12:54:47 -0700

    server now sends data from the "Game" database to the front end

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 /* Main server that handles requests from twitch and serves the state that
-   each streamer is in. Queries the Game database */
+ * each streamer is in. Queries the Game database */
 var http = require('http');
+var mongoClient = require('mongodb').MongoClient;
+var database = "mongodb://localhost:27017/riotContest";
+var dbGame = "Game";
 
-//listen for request from front end
-//look up twitch username (gotten from the jquery) in the Game database
+http.createServer(function(req, res) {
+   /* go to the "Game" database and pull the data currently there */
+   fetchGames(res);
+}).listen(8000, 'localhost');
 
+function fetchGames(res) {
+   mongoClient.connect(database, function(err, db) {
+      if(err) {
+
+      }
+      var collection = db.collection(dbGame);
+      var gameCursor = collection.find();
+      gameCursor.toArray(function(err, docs) {
+         if(err) {
+
+         }
+         writeGameData(docs, res);
+         db.close();
+      });
+   });
+}
+
+function writeGameData(docs, res) {
+   res.writeHead(200, {'Content-Type' : 'tapplication/json'});
+   res.end(JSON.stringify(docs));
+}
